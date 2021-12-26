fd9df7595cab840487650382a2b115b53907e0ed
AhmedRabea0302 <arabea169@gmail.com>
2021-12-26 21:05:23 +0200

    Adding rhe single project Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"/>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <script>
 
+import SingleProject from "../components/SingleProject.vue"
 export default {
+
   name: 'Home',
   components: {
+    SingleProject
+  },
+
+  data() {
+    return {
+      projects: [],
+    }
+  },
+
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(response => response.json())
+    .then(response => this.projects = response)
+    .catch(error => console.log(error))
   }
 }
 </script>
