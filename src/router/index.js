262b5637a115465da0829d58625fd9626a388114
AhmedRabea0302 <arabea169@gmail.com>
2021-12-26 22:41:45 +0200

    Create the Add Project Form

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import AddProject from '../views/AddProject'
 const routes = [{
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/add',
+        name: 'AddProject',
+        component: AddProject
     },
 
 ]
---
+++ src/views/AddProject.vue
@@ -0,0 +1,72 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <label for="title">Title</label>
+      <input type="text" required v-model="title" />
+
+      <label for="details">Details</label>
+      <textarea required v-model="details"></textarea>
+
+      <button type="">Add Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: '',
+            details: '',
+        }
+    },
+
+    handleSubmit() {
+        console.log(this.title);
+        console.log(this.details);
+    }
+}
+</script>
+
+<style>
+    form {
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 10px;
+    }
+
+    label {
+        display: block;
+        color: #bbb;
+        text-transform: uppercase;
+        font-size: 14px;
+        font-weight: bold;
+        letter-spacing: 1px;
+        margin: 20px 0 10px 0;
+    }
+
+    input {
+        padding: 10px;
+        border: 0;
+        border-bottom: 1px solid #ddd;
+        width: 100%;
+        box-sizing: border-box;
+    }
+
+    textarea {
+        border: 1px solid #ddd;
+        padding: 10px;
+        width: 100%;
+        box-sizing: border-box;
+        height: 100px;
+    }
+
+    form button {
+        margin: 20px auto 0;
+        display: block;
+        background-color: #00ce89;
+        color: white;
+        padding: 10px;
+        border: 0;
+        border-radius: 6px;
+        font-size: 16px;
+    }
+</style>
